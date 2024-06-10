b72048afb05a555a219b71002a5cadebfe7ca54e
kymy44 <54514007+kymy44@users.noreply.github.com>
2024-06-10 12:17:03 +0100

    CORS headers aplicados

--- epubReader/settings.py
+++ epubReader/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     "fileAdmin",
     "rest_framework",
     "drf_yasg",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
